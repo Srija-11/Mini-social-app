ae79b10477a34702c1f0df68659b3281b5a5d489
Srija Ghorui <165726248+Srija-11@users.noreply.github.com>
2025-05-07 14:47:18 +0530

    Add files via upload

---
+++ models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    user:
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"post"
+        },
+        date:{
+            type:Date,
+            default:Date.now
+        },
+        content:String,
+        likes:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"post"
+        }]
+})
+
+module.exports = mongoose.model('post', postSchema);
